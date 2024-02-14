02079091ab1b700176f45e3a7d7176cc720c245c
Omar Pervez <freelancertusar5182@gmail.com>
2024-02-14 19:32:38 +0600

    updated the repo

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Project Title
+
+CT Skill test
+
+## Project Structure
+
+The project structure is organized as follows:
+
+- `index.html`: The main HTML file that integrates the enhanced layout.
+- `assets/`: Contains assets such as images.
+  - `resources/`: Custom JavaScript and CSS files.
+    - `app.js`: Contains custom JavaScript code for interactive features.
+    - `style.css`: Contains custom CSS styles for enhancing the layout.
+
+
+## Technologies Used
+
+- HTML: Markup language for structuring content.
+- CSS: Used for styling and layout.
+- JavaScript/jQuery: Implemented for dynamic behavior and interactive features.
+
+### Third-Party Enhancements
+
+This project leverages several third-party tools and frameworks to elevate its user interface and functionality:
+
+#### Normalize.css
+- A CSS reset library for consistent styling across various browsers.
+
+#### jQuery
+- A versatile JavaScript library for simplified DOM manipulation, event handling, and animation.
+
+#### Slick.js
+- A responsive carousel jQuery plugin for creating slick and flexible sliders.
+
+#### Google Fonts
+- Utilized Google Fonts to enhance the typography and visual appeal of the web page.
+
+#### Custom Styles
+- **styles.css**: Custom CSS styles tailored to enhance the user interface of the CT Skill test.
+- **global.css**: Custom CSS file for global styles.
+
+
+#### Custom JavaScript Modules
+- **app.js**: Custom JavaScript file containing interactive functionality for the CT Skill test.
--- resource/js/app.js
+++ resource/js/app.js
@@ -1,13 +1,92 @@
-import './sticky-header.js';
-import './carousel-initilization.js';
-import './tab.js'
+// sticky header
+window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+ 
+    // Select the header element using its "header" tag name
+    const header = document.querySelector("header");
+    const logo = header.querySelector('#header-logo');
+    const isHeaderSticky = header.classList.contains('sticky-header');
+ 
+    // Check if the scroll position is greater than 20 pixels
+    if (scroll > 20 && !isHeaderSticky) {
+       header.classList.add('sticky-header');
+       logo.src = './assets/logo-2.png';
+       logo.classList.add('sticky-logo');
+    } else if (scroll <= 20 && isHeaderSticky) {
+       header.classList.remove('sticky-header');
+       logo.src = './assets/header-logo.png';
+       logo.classList.remove('sticky-logo');
+    }
+ });
+ 
+ 
+ document.querySelectorAll('.nav-link').forEach(function (navLink) {
+    navLink.addEventListener('click', function (event) {
+       event.preventDefault();
+ 
+       const targetId = this.getAttribute('href').substring(1);
+       const targetElement = document.getElementById(targetId);
+ 
+       if (targetElement) {
+          const targetOffset = targetElement.offsetTop;
+ 
+          // Smooth scroll
+          window.scrollTo({
+             top: targetOffset,
+             behavior: 'smooth'
+          });
+       }
+    });
+ });
 
+ // slick slider functionality
+ $(document).ready(function () {
+    $('.carousel').slick({
+       centerMode: false,
+ 
+       slidesToShow: 2,
+       dots: true,
+       arrows: false,
+       variableWidth: true,
+       responsive: [{
+             breakpoint: 768,
+             settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 3
+             }
+          },
+          {
+             breakpoint: 480,
+             settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 1
+             }
+          }
+       ],
+ 
+    });
+ });
+ 
 
-
-
-
-
-
-
-
-
+ // tab js
+ const mountain_1 = document.getElementById('mountain-1');
+ const mountain_2 = document.getElementById('mountain-2');
+ const tab_1 = document.getElementById("tab-1");
+ const tab_2 = document.getElementById("tab-2");
+ 
+ function switchTab(tabToShow, tabToHide, activeMountain, inactiveMountain) {
+    tabToShow.style.display = 'block';
+    tabToHide.style.display = 'none';
+    activeMountain.classList.add('active');
+    inactiveMountain.classList.remove('active');
+ }
+ 
+ mountain_1.addEventListener('click', () => {
+    switchTab(tab_1, tab_2, mountain_1, mountain_2);
+ });
+ 
+ mountain_2.addEventListener('click', () => {
+    switchTab(tab_2, tab_1, mountain_2, mountain_1);
+ });
